d70265b631d0a008e733f837cf08cf872b9717ff
nadiia-dev <nadiia.pavliuchenko.dev@gmail.com>
2025-06-12 17:06:34 +0300

    init react project && add graphql

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { Card, MantineProvider, Text } from "@mantine/core";
+
+function App() {
+  return (
+    <MantineProvider>
+      <Card shadow="lg">
+        <Text>Hello</Text>
+      </Card>
+    </MantineProvider>
+  );
+}
+
+export default App;
