b4e7809c8f5a907b0b59a4dacfb35739bbc06cd6
nadiia-dev <nadiia.pavliuchenko.dev@gmail.com>
2025-06-12 16:48:08 +0300

    add user actions

--- backend/src/user/user.resolver.ts
+++ backend/src/user/user.resolver.ts
@@ -1,4 +1,39 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Context, Mutation, Resolver } from '@nestjs/graphql';
+import { UserService } from './user.service';
+import { UseGuards } from '@nestjs/common';
+import { GraphqlAuthGuard } from 'src/auth/graphql-auth.guard';
+import { User } from './user.type';
+import { Request } from 'express';
+// @ts-ignore
+import { GraphQLUpload, FileUpload } from 'graphql-upload';
+import { join } from 'path';
+import * as fs from 'fs';
+import { v4 as uuidv4 } from 'uuid';
 
 @Resolver()
-export class UserResolver {}
+export class UserResolver {
+  constructor(private readonly userService: UserService) {}
+
+  @UseGuards(GraphqlAuthGuard)
+  @Mutation(() => User)
+  async updateProfile(
+    @Args('fullname') fullname: string,
+    @Args('file', { type: () => GraphQLUpload, nullable: true })
+    file: FileUpload,
+    @Context() context: { req: Request },
+  ) {
+    const imageUrl = file ? await this.storeImageAndGetUrl(file) : undefined;
+    const userId = context.req.user?.sub;
+    return this.userService.updateUserProfile(userId!, fullname, imageUrl);
+  }
+
+  private async storeImageAndGetUrl(file: GraphQLUpload) {
+    const { createReadStream, filename } = await file;
+    const uniqueFilename = `${uuidv4()}_${filename}`;
+    const imagePath = join(process.cwd(), 'public', 'images', uniqueFilename);
+    const imageUrl = `${process.env.APP_URL}/images/${uniqueFilename}`;
+    const readStream = createReadStream();
+    readStream.pipe(fs.createWriteStream(imagePath));
+    return imageUrl;
+  }
+}
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,4 +1,66 @@
 import { Injectable } from '@nestjs/common';
+import { join } from 'path';
+import { PrismaService } from 'src/prisma.service';
+import * as fs from 'fs';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async updateUserProfile(
+    userId: number,
+    fullname: string,
+    avatarUrl?: string,
+  ) {
+    if (avatarUrl) {
+      const oldUserData = await this.prisma.user.findUnique({
+        where: {
+          id: userId,
+        },
+      });
+
+      const updatedUser = await this.prisma.user.update({
+        where: { id: userId },
+        data: {
+          fullname,
+          avatarUrl,
+        },
+      });
+
+      if (oldUserData?.avatarUrl) {
+        const name = oldUserData?.avatarUrl.split('/').pop() as string;
+        const path = join(__dirname, '..', '..', 'public', 'images', name);
+        if (fs.existsSync(path)) {
+          fs.unlinkSync(path);
+        }
+      }
+      return updatedUser;
+    }
+
+    return await this.prisma.user.update({
+      where: { id: userId },
+      data: { fullname },
+    });
+  }
+
+  async searchUsers(fullname: string, userId: number) {
+    return await this.prisma.user.findMany({
+      where: {
+        fullname: {
+          contains: fullname,
+        },
+        id: {
+          not: userId,
+        },
+      },
+    });
+  }
+
+  async getUser(userId: number) {
+    return await this.prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+  }
+}
